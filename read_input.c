c075dcbc07cea92f288b2f86de7cc606579ac032
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-04 17:29:14 +1000

    Update read_input.c

--- read_input.c
+++ read_input.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * read_input - Handles reading input from stdin, with prompt if interactive.
+ * @line: Pointer to the input buffer.
+ * @len: Size of the input buffer.
+ * @interactive: 1 if shell is interactive, 0 if non-interactive.
+ *
+ * Return: 0 on success, -1 on failure or EOF.
+ */
 int read_input(char **line, size_t *len, int interactive)
 {
 	ssize_t rd;
