3f2e6037e95f0595d09265ceef515d371a9ae88e
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-04 17:32:31 +1000

    Update command.c

--- command.c
+++ command.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * command - Handles built-in shell commands like 'exit' and 'env'.
+ * @arg: Parsed command arguments.
+ * @line: Original input line (used for memory cleanup).
+ * @status: Exit status of last executed command.
+ *
+ * Return: 1 if a built-in command was executed, 0 otherwise.
+ */
+
 int command(char **arg, char *line, int status)
 {
 	int i = 0;
