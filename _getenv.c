54845b1fd895df9858be2f4f12dc8eb7bc69a722
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-04 17:33:14 +1000

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _getenv - Retrieves the value of an environment variable.
+ * @name: The name of the environment variable.
+ *
+ * Return: Pointer to the value, or NULL if not found.
+ */
+
 char *_getenv(const char *name)
 {
 	int i = 0;
